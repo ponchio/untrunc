44fcb82909b6c324936711058d138511f3de4d54
ponchio <ponchio@gmail.com>
2021-04-13 13:39:53 +0200

    small samples for text codec now matched.

--- codec_text.cpp
+++ codec_text.cpp
@@ -15,6 +15,8 @@
 
 Match Codec::textMatch(const unsigned char *start, int maxlength) {
 	Match match;
+
+
 	//TODO find more sensible values for these maxes
 	uint32_t max_text_length = std::min(maxlength, 4096);
 	uint32_t max_atom_length = 4096; //?
@@ -35,14 +37,16 @@ Match Codec::textMatch(const unsigned char *start, int maxlength) {
 
 	offset += size + 2; //length does not include size word
 
-	//TODO if text is ascii changes might be significantly higher!
 
+	//TODO if text is ascii changes might be significantly higher!
+	//small text or already seen beginnings.
 	int32_t begin = readBE<int32_t>(start);
-	if(stats.fixed_size && stats.beginnings32.count(begin)) {
-		match.chances =stats.beginnings32[begin];
+	if(size < 128 || stats.beginnings32.count(begin)) {
+		match.chances = stats.beginnings32[begin];
 		match.length = size+2;
 		return match;
 	}
+
 
 	const char *atoms[9] = { "encd", "styl", "ftab", "hlit", "hclr", "drpo", "drpt", "imag", "metr" };
 	while(offset < maxlength - 10) { //shortest atom has length(4) name(4) shadow transparency(2)
